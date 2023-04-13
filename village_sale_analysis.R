a36b4a9274d179d0518bb07920060a3e62acd322
Art Steinmetz <apsteinmetz@yahoo.com>
2023-04-12 21:52:11 -0400

    trying different models

--- village_sale_analysis.R
+++ village_sale_analysis.R
@@ -6,6 +6,8 @@
 
 
 library(tidyverse)
+library(tidymodels)
+library(performance)
 
 fix_col_strings = function(str_vec){
   str_vec |> 
@@ -70,7 +72,7 @@ prop_data <- combined_raw_data |>
   separate(building_class_category,into = c("building_category","building_category_name"),extra = "merge") |> 
   mutate(building_category = as.numeric(building_category)) |>
   mutate(is_single_family = (building_category == 1)) |> 
-  mutate(has_commericial = (commercial_units > 0)) |> 
+  mutate(has_commercial = (commercial_units > 0)) |> 
   identity()
 
 save(prop_data,file="data/prop_data.rdata")
@@ -80,31 +82,154 @@ max_unit_size <-  6
 min_sale_price <- 1000000
 min_sq_feet <- 1000
 max_sq_feet <- 15000
-valid_categories <- c(1:8,14)
+valid_categories <- c(1:8)
 
 
 # isolate zips buildings of interest
 village_data <- prop_data |> 
+  filter(land_square_feet < 10000) |> 
   filter(zip_code %in% village_zips) |> 
+  filter(borough %in% c("1","MANHATTAN")) |> 
+  filter(building_category %in% valid_categories) |>
   filter(sale_price >= min_sale_price) |> 
   filter(gross_square_feet >= min_sq_feet) |> 
   filter(gross_square_feet <= max_sq_feet) |> 
   filter(residential_units > 0) |> 
   filter(residential_units <= max_unit_size) |> 
-  filter(building_category %in% valid_categories) |>
   filter(is.na(apartment_number)) |> # whole building
-#  filter(year(sale_date) > 2020) |> 
   identity()
 
-village_data |> ggplot(aes(gross_square_feet,sale_price)) + geom_point() +
+village_data |> 
+  # filter(year(sale_date)> 2011) |> 
+  ggplot(aes(gross_square_feet,sale_price,color = is_single_family)) + geom_point() +
   geom_smooth() +
   scale_y_continuous(labels = scales::dollar) + 
-  scale_x_continuous(labels = scales::comma)
+  scale_x_continuous(labels = scales::comma) + 
+  labs(title = "Sale Price Vs. Size",
+       subtitle = "Townhouses in GV and Chelsea Sold from 2012-2022")
 
 village_data |> 
   filter(gross_square_feet > 4500) |> 
   filter(gross_square_feet < 5500) |> 
-  filter(building_category == 1) |> 
-  ggplot(aes(sale_date,sale_price,color = neighborhood)) + geom_point() + 
+  ggplot(aes(sale_date,sale_price,color = is_single_family)) + geom_point() + 
   scale_y_continuous(labels = scales::dollar) + 
-  geom_smooth()
+  geom_smooth() + 
+  labs(title = "Sale Price Over Time",
+       subtitle = "Townhouses between 4,500 and 5,000 Square Feet in GV and Chelsea")
+
+featured_data <- village_data |>
+  select(
+    sale_price,
+    sale_date,
+    gross_square_feet,
+#    land_square_feet,
+    neighborhood,
+    is_single_family,
+    has_commercial
+  )
+
+
+model1 <- lm(sale_price ~ .,data = featured_data)
+
+summary(model1)
+glance(model1)
+broom::tidy(model1)
+check_model(model1)
+
+
+# TEST single variable
+featured_data |> 
+  ggplot(aes(land_square_feet,sale_price)) + geom_point() + geom_smooth()
+
+one_house_before <- village_data |> filter(str_detect(address,"133")) |> 
+  select(
+  sale_price,
+  sale_date,
+  gross_square_feet,
+  land_square_feet,
+  neighborhood,
+  is_single_family,
+  has_commercial
+) 
+
+one_house_after <- one_house_before |> 
+  mutate(sale_date = Sys.Date()) |> 
+  mutate(is_single_family = TRUE) |> 
+  mutate(gross_square_feet = 5188)
+
+predict(model1,newdata = one_house_before)
+predict(model1,newdata = one_house_after)
+
+goodness <- tibble(predicted = predict(model1,data = featured_data),
+                   actual = featured_data$sale_price)
+
+goodness |> 
+  ggplot(aes(actual,predicted)) + geom_point() +
+  geom_smooth(method = "lm") + 
+  geom_abline(slope=1,intercept = 0) +
+  scale_y_continuous(labels = scales::dollar) + 
+  scale_x_continuous(labels = scales::dollar)
+
+
+model1_rmse <- sqrt(sum((goodness$predicted - goodness$actual)^2)/
+                      nrow(goodness))
+model1_rmse
+
+
+model2 <- ranger::ranger(sale_price ~.,
+                         data = featured_data,
+                         importance = "impurity")
+
+
+
+vip::vip(model2)
+goodness <- tibble(predicted = predict(model2,data = featured_data)$prediction,
+                   actual = featured_data$sale_price)
+model2_rmse <- sqrt(sum((goodness$predicted - goodness$actual)^2)/
+  nrow(goodness))
+model2_rmse
+
+goodness |> 
+  ggplot(aes(actual,predicted)) + geom_point() +
+  geom_smooth(method = "lm") + 
+  geom_abline(slope=1,intercept = 0) +
+  scale_y_continuous(labels = scales::dollar) + 
+  scale_x_continuous(labels = scales::dollar)
+  
+
+predict(model2,data = one_house_before)$prediction
+predict(model2,data = one_house_after)$prediction
+
+# ---------------
+# tidy models
+
+ranger_spec <- rand_forest() |> 
+  set_engine("ranger",importance = "impurity") |> 
+  set_mode("regression") |> 
+  step_log(gross_square_feet) |> 
+  step_dummy(all_nominal_predictors())
+
+rec <- recipe(sale_price ~.,data = featured_data)
+
+wf <- workflow() |> 
+add_model(ranger_spec) |> 
+add_recipe(rec) 
+
+prop_fit <- wf |> fit(featured_data)
+
+goodness <- tibble(predicted = predict(prop_fit,featured_data)$.pred,
+                   actual = featured_data$sale_price)
+goodness
+
+model3_rmse <- sqrt(sum((goodness$predicted - goodness$actual)^2)/
+                      nrow(goodness))
+model3_rmse
+
+goodness |> 
+  ggplot(aes(actual,predicted)) + geom_point() +
+  geom_smooth(method = "loess") + 
+  geom_abline(slope=1,intercept = 0) +
+  scale_y_continuous(labels = scales::dollar) + 
+  scale_x_continuous(labels = scales::dollar)
+
+
